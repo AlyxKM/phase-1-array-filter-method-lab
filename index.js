bf0cc11081f81a88d69447eb555378e9ce9836a9
alyxandria meadows <alyxmeadows@yahoo.com>
2021-08-15 15:45:54 -0500

    commit tree c0f251e5481e7dbb2da69ef41e8808a2c9dba3ba

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter( matchingDrivers => matchingDrivers.toLowerCase() === name.toLowerCase()
+    )
+};
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter( matchingDrivers => matchingDrivers.toLowerCase().indexOf(letters.toLowerCase()) === 0
+    )
+};
+
+function matchName(drivers, name){
+    return drivers.filter( matchingDriver => matchingDriver.name === name)
+
+};
+
+
